cc3cf0f01e845d04ce028b1435eef2c8727ab9b1
Abhinav4518 <146935696+Abhinav4518@users.noreply.github.com>
2024-12-22 07:43:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import Abhi from "./abhi.js";
+function App() {
+  return (
+    <>
+      <Abhi />
+      <h1>Nahi baat karni hai mujhse ?</h1>
+      <p>Okay, Katti hun mai tujhse. Don't talk to me.</p>
+    </>  
+  );
+}
+
+export default App;
---
+++ abhi.js
@@ -0,0 +1,6 @@
+function Abhi(){
+    return (
+        <h1>Aur Kya haal hai beta ji. Batao bhai kya hua hai aapko.</h1>
+    )
+}
+export default Abhi;
